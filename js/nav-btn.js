a3beaeb12c5994c5b3b724083f26b62b436e6fd5
Liakh Vladyslav <wlodzislaw.lw@gmail.com>
2018-02-13 01:44:09 +0200

    v0.8
    Fixed barba.js
    added new transitions

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -2,14 +2,6 @@ $('.menu-toggle').on('click', function() {
   $('.full-menu').toggleClass('full-menu--open');
 })
 
-$(document).ready(function() {
   $('.menu-toggle').click(function() {
     $(this).toggleClass('open');
   });
-});
-
-$(document).ready(function() {
-  $('.link-btn').on('click', function() {
-    $('.menu-toggle').toggleClass('open');
-  });
-});
