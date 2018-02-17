413f129a2bf3a80f7defe535ddc75dfe15e44d4b
Liakh Vladyslav <wlodzislaw.lw@gmail.com>
2018-02-17 18:08:45 +0200

    v0.8,6
    Parallax

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -7,35 +7,35 @@ $('.menu-toggle').click(function() {
 })
 
 $('#btn1').click(function(e) {
-   e.preventDefault();
-  $(this).toggleClass('active-page').delay( 800 );
-  $('#btn2').removeClass('active-page').delay( 800 );
-  $('#btn3').removeClass('active-page').delay( 800 );
-  $('#btn4').removeClass('active-page').delay( 800 );
+  e.preventDefault();
+  $(this).toggleClass('active-page').delay(800);
+  $('#btn2').removeClass('active-page').delay(800);
+  $('#btn3').removeClass('active-page').delay(800);
+  $('#btn4').removeClass('active-page').delay(800);
 })
 
 $('#btn2').click(function(e) {
-   e.preventDefault();
-  $(this).toggleClass('active-page').delay( 800 );
-  $('#btn1').removeClass('active-page').delay( 800 );
-  $('#btn3').removeClass('active-page').delay( 800 );
-  $('#btn4').removeClass('active-page').delay( 800 );
+  e.preventDefault();
+  $(this).toggleClass('active-page').delay(800);
+  $('#btn1').removeClass('active-page').delay(800);
+  $('#btn3').removeClass('active-page').delay(800);
+  $('#btn4').removeClass('active-page').delay(800);
 })
 
 $('#btn3').click(function(e) {
-   e.preventDefault();
-  $(this).toggleClass('active-page').delay( 800 );
-  $('#btn2').removeClass('active-page').delay( 800 );
-  $('#btn1').removeClass('active-page').delay( 800 );
-  $('#btn4').removeClass('active-page').delay( 800 );
+  e.preventDefault();
+  $(this).toggleClass('active-page').delay(800);
+  $('#btn2').removeClass('active-page').delay(800);
+  $('#btn1').removeClass('active-page').delay(800);
+  $('#btn4').removeClass('active-page').delay(800);
 })
 
 $('#btn4').click(function(e) {
-   e.preventDefault();
-  $(this).toggleClass('active-page').delay( 800 );
-  $('#btn2').removeClass('active-page').delay( 800 );
-  $('#btn3').removeClass('active-page').delay( 800 );
-  $('#btn1').removeClass('active-page').delay( 800 );
+  e.preventDefault();
+  $(this).toggleClass('active-page').delay(800);
+  $('#btn2').removeClass('active-page').delay(800);
+  $('#btn3').removeClass('active-page').delay(800);
+  $('#btn1').removeClass('active-page').delay(800);
 })
 
 var anim = bodymovin.loadAnimation({
@@ -53,4 +53,13 @@ $(document).ready(function() {
       resolve(true);
     }
   })
+});
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if (window.matchMedia('(min-width: 768px)').matches) {
+      $('.right').css('transform', 'translate3d(0,' + $(this).scrollTop() * 2 + 'px, 0)');
+    }
+  }).scroll();
+
 });
